33e69017fa76329ca2c27fcf6bce20c71cab9fa1
vaibhavkls <vaibhav@knightlearningsolutions.com>
2021-04-26 15:29:14 +0530

    added a minus feature

--- basicjava/AddNumbers.java
+++ basicjava/AddNumbers.java
@@ -3,7 +3,9 @@ public class AddNumbers {
 		int num1 = 1; 
 		int num2 = 2;
 		int sum = num1 + num2;
+		int minus = num1 - num2;
 		System.out.println("1+2 = " + sum);
+		System.out.println("1-2 = " + minus);
 	}
 
 }
